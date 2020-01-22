bef581b594f327cb9e37ac88e11e7f26c7381563
jmnsingle <jmnogueira2011@gmail.com>
2020-01-21 22:02:47 -0300

    Push Notification implemented.

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CodePush from 'react-native-code-push';
+import OneSignal from 'react-native-onesignal'; 
 import { StatusBar, YellowBox } from 'react-native';
 
 import Routes from './src/routes';
@@ -8,13 +9,42 @@ YellowBox.ignoreWarnings([
   'Unrecognized WebSocket'
 ]);
 
-function App() {
-  return (
-    <>
-      <StatusBar backgroundColor="#7d40e7" barStyle="light-content" />
-      <Routes />
-    </>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    OneSignal.init('c5a844fb-b57d-4b4c-bd15-88c26cb96864');
+    
+    OneSignal.addEventListener('received', this.onReceived );
+    OneSignal.addEventListener('opened', this.onOpened );
+    OneSignal.addEventListener('ids', this.onIds );
+
+  }
+
+  componentWillUnmount() {
+    OneSignal.addEventListener('received', this.onReceived );
+    OneSignal.addEventListener('opened', this.onOpened );
+    OneSignal.addEventListener('ids', this.onIds );
+  }
+
+  onReceived = (data) => {
+
+  }
+
+  onOpened = (notification) => {
+
+  }
+
+  onIds = id => {}
+
+  render() {
+    return (
+      <>
+        <StatusBar backgroundColor="#7d40e7" barStyle="light-content" />
+        <Routes />
+      </>
+    );
+  }
 }
 
 export default CodePush({
